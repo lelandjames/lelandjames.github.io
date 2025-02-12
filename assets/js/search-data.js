3f914dfd487ae87623641fa4ab38c717e1d58378
Justin Foreman <jforeman@nano.local>
2025-02-12 11:35:49 -0500

    updates [ci skip]

---
+++ assets/js/search-data.js
@@ -0,0 +1,103 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "HOME",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/new-site/";
+    },
+  },{id: "dropdown-enworld",
+              title: "ENWORLD",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-animal-land",
+              title: "ANIMAL LAND",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-longberry-39-s-leap",
+              title: "LONGBERRY&#39;S LEAP",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-a-mice-christmas",
+              title: "A MICE CHRISTMAS",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-the-giving-secret",
+              title: "THE GIVING SECRET",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-about",
+              title: "ABOUT",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-rollercoaster-moons",
+              title: "ROLLERCOASTER MOONS",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-everything-dances",
+              title: "EVERYTHING DANCES",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-muse",
+              title: "MUSE",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-what-god-in-this",
+              title: "WHAT GOD IN THIS...",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-gypsy-cab",
+              title: "GYPSY CAB",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-my-brother-39-s-keeper",
+              title: "MY BROTHER&#39;S KEEPER",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },{id: "dropdown-placeholder",
+              title: "PLACEHOLDER",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "";
+              },
+            },];
